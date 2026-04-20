334719d89e2f69729161b3caf1b7390d9cad676e
Xifan1554 <576000254@qq.com>
2026-04-20 17:05:44 +0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🎉 我的网站上线了！</h1>
+      <p>欢迎访问，这是通过 Vercel 部署的 Next.js 项目。</p>
+    </div>
+  );
+}
